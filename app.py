b6cd657ee06900a4bf9a50abeed7530d14fe6870
bryanchau <nchau1@student.gsu.edu>
2021-11-02 17:36:56 +0000

    Adding login/signup page and setting up heroku database

--- app.py
+++ app.py
@@ -1,58 +1,126 @@
 import flask
-from flask_login.utils import login_required
 import os
 import json
+from dotenv import load_dotenv, find_dotenv
+from flask_login import login_user, current_user, LoginManager, logout_user, login_required
+import random
+import base64
+import requests
+import urllib.request
+from flask import redirect
 
+load_dotenv(find_dotenv())
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
-app = flask.Flask(__name__, static_folder='./build/static')
-# This tells our Flask app to look at the results of `npm build` instead of the 
+app = flask.Flask(__name__, static_folder="./build/static")
+# This tells our Flask app to look at the results of `npm build` instead of the
 # actual files in /templates when we're looking for the index page file. This allows
 # us to load React code into a webpage. Look up create-react-app for more reading on
 # why this is necessary.
+db_url = os.getenv("DATABASE_URL")
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+# Gets rid of a warning
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = os.getenv("SECRETKEY")  # don't defraud my app ok?
+
+db = SQLAlchemy(app)
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+    def get_username(self):
+        return self.username
+
+
+db.create_all()
+
 bp = flask.Blueprint("bp", __name__, template_folder="./build")
 
-@bp.route('/index')
+
+@bp.route("/index")
 @login_required
 def index():
     # TODO: insert the data fetched by your app main page here as a JSON
-    DATA = {"your": "data here"}
+    DATA = {"current_user": current_user.username}
     data = json.dumps(DATA)
     return flask.render_template(
         "index.html",
         data=data,
     )
+@app.route("/logout", methods=["POST"])
+@login_required
+def logout():
+    """This function will logout user
+    Returns:
+        / endpoint
+    """
+    logout_user()
+    return redirect("/")
 
 app.register_blueprint(bp)
 
-@app.route('/signup')
+
+login_manager = LoginManager()
+login_manager.login_view = "login"
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_name):
+    return User.query.get(user_name)
+
+
+@app.route("/signup")
 def signup():
-	...
+    return flask.render_template("signup.html")
 
-@app.route('/signup', methods=["POST"])
+
+@app.route("/signup", methods=["POST"])
 def signup_post():
-	...
+    username = flask.request.form.get("username")
+    user = User.query.filter_by(username=username).first()
+    if user:
+        pass
+    else:
+        user = User(username=username)
+        db.session.add(user)
+        db.session.commit()
 
-@app.route('/login')
+    return flask.redirect(flask.url_for("login"))
+
+
+@app.route("/login")
 def login():
-    ...
+    return flask.render_template("login.html")
 
-@app.route('/login', methods=["POST"])
+
+@app.route("/login", methods=["POST"])
 def login_post():
-	...
+    username = flask.request.form.get("username")
+    user = User.query.filter_by(username=username).first()
+    if user:
+        login_user(user)
+        return flask.redirect(flask.url_for("bp.index"))
 
-@app.route('/save', methods=["POST"])
-def save():
-    ...
+    else:
+        return flask.jsonify({"status": 401, "reason": "Username or Password Error"})
 
 
-@app.route('/')
+@app.route("/")
 def main():
-	...
-
-
-
+    if current_user.is_authenticated:
+        return flask.redirect(flask.url_for("bp.index"))
+    return flask.redirect(flask.url_for("login"))
 
 app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8081)),
+    host=os.getenv("IP", "0.0.0.0"),
+    port=int(os.getenv("PORT", 8081)),
 )
