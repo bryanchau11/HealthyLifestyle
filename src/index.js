c11af7f8e6bb1bb13ade85f7fb63a656733af0f7
bryanchau <nchau1@student.gsu.edu>
2021-11-04 03:09:57 +0000

    Finished setting up react routes for main page.

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Navigation';
 import NotFoundPage from './pages/NotFound';
-const routing  = ( 
+import Users from './components/User';
+import Contact from './components/Contact';
+const routing = (
   <Router>
     <div>
-      <Header/>
+      <Header />
       <Routes>
-        <Route exact path="/index" component={App}/>
-        <Route component={NotFoundPage} />
+        <Route path="/index" element={<App />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route element={<NotFoundPage />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   </Router>
- 
 );
-ReactDOM.render(
-  routing,
-  document.getElementById('root')
-);
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
