5e10a85b0f5f5395feaa6d073fba6fb556f624d0
Ubuntu <mkhaalik@SWE-A-Machine.0rbwy4qc221upj41y1wofjpogh.bx.internal.cloudapp.net>
2021-11-05 22:36:02 +0000

    emailjs set up complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
-function Contact() {
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('healthyLifestyle', 'contact_healthyLifestyle', form.current, 'user_iHEKSYbKPnoYyob6LLTdv')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <div>
-      <h1>This is Contact page</h1>
+      <h1>Contact Us</h1>
+      <form ref={form} onSubmit={sendEmail}/>
+      <label>Name:</label>
+      <input type="text" name="from_name" />
+      <br/><br/>
+      <label>Email:</label>
+      <input type="email" name="user_email" />
+      <br/><br/>
+      <label>Message:</label>
+      <textarea name="message" />
+      <button onClick={sendEmail}>Send</button>
     </div>
   );
-}
+};
 
 export default Contact;
+
