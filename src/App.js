ff762149d829a2ca3d181521303b3a5de736068f
bryanchau <nchau1@student.gsu.edu>
2021-11-06 03:50:12 +0000

    Polishing the app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useRef } from 'react';
+
 import { Link } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import { useScroll } from 'react-use-gesture';
+import { animated, useSpring } from 'react-spring';
+
 function App() {
-  // fetches JSON data passed in by flask.render_template and loaded
-  // in public/index.html in the script with id "data"
   const args = JSON.parse(document.getElementById('data').text);
   const list = args.list_of_item;
+  const [style, set] = useSpring(() => ({
+    transform: 'perspective(300px) rotateY(0deg)',
+  }));
+
+  const bind = useScroll((event) => {
+    set({
+      transform: `perspective(500px) rotateY(${event.scrolling ? event.delta[0] : 0}deg)`,
+    });
+  });
+
   return (
     <>
-      <h1> 10 Random Meals</h1>
-      <ul>
-        {list.map((item) => (
-          <li key={item.food}>
+      <div id="randomMeal">
+        <h1>Pick your category</h1>
+        <div className="container" {...bind()}>
+          {list.map((item) => (
             <Nav.Link as={Link} to={`/recipe/${item.food}`}>
-              <div>{item.food}</div>
-              <div>
-                <img className="fixed_img" src={item.image} alt="food pictures" />{' '}
-              </div>
+              <animated.div
+                key={item.food}
+                className="card"
+                style={{
+                  ...style,
+                  backgroundImage: `url(${item.image})`,
+                }}
+              >
+                {item.food}
+              </animated.div>
             </Nav.Link>
-          </li>
-        ))}
-      </ul>
+          ))}
+        </div>
+      </div>
+      <div id="randomMeal">
+        <h1>10 Random Meals</h1>
+        <div className="container" {...bind()}>
+          {list.map((item) => (
+            <Nav.Link as={Link} to={`/recipe/${item.food}`}>
+              <animated.div
+                key={item.food}
+                className="card"
+                style={{
+                  ...style,
+                  backgroundImage: `url(${item.image})`,
+                }}
+              >
+                {item.food}
+              </animated.div>
+            </Nav.Link>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Nav, Navbar, Container, Button, Form, FormControl } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import '../App.css';
-
+import '../css/Header.css';
 function Header() {
   const args = JSON.parse(document.getElementById('data').text);
 
@@ -11,6 +10,15 @@ function Header() {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
+          <div class="wrapper">
+            <span>W</span>
+            <span>E</span>
+            <span>L</span>
+            <span>C</span>
+            <span>O</span>
+            <span>M</span>
+            <span>E</span>
+          </div>
           <Navbar.Brand>Welcome {args.current_user}</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={NavLink} to="/index">
