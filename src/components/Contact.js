9e51132ec670ff0302282035a78963efd6886726
Ubuntu <mkhaalik@SWE-A-Machine.0rbwy4qc221upj41y1wofjpogh.bx.internal.cloudapp.net>
2021-11-06 01:27:09 +0000

    working contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,10 +6,11 @@ export const Contact = () => {
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm('healthyLifestyle', 'contact_healthyLifestyle', form.current, 'user_iHEKSYbKPnoYyob6LLTdv')
+    
+    emailjs.sendForm('healthyLifestyle', 'contact_healthyLifestyle',form.current, 'user_iHEKSYbKPnoYyob6LLTdv')
       .then((result) => {
           console.log(result.text);
+         
       }, (error) => {
           console.log(error.text);
       });
@@ -17,16 +18,17 @@ export const Contact = () => {
   return (
     <div>
       <h1>Contact Us</h1>
-      <form ref={form} onSubmit={sendEmail}/>
+      <form onSubmit={sendEmail} ref={form}>
       <label>Name:</label>
-      <input type="text" name="from_name" />
+      <input type="text" name="form_name"/>
       <br/><br/>
       <label>Email:</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="user_email"/>
       <br/><br/>
       <label>Message:</label>
       <textarea name="message" />
-      <button onClick={sendEmail}>Send</button>
+      <input type="submit" name="Send" value="Send"/>
+      </form>
     </div>
   );
 };
