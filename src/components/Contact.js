5f9ab46b103a5168818a7ebde7bb25c64ba28e64
Ubuntu <mkhaalik@SWE-A-Machine.0rbwy4qc221upj41y1wofjpogh.bx.internal.cloudapp.net>
2021-11-06 02:34:47 +0000

    aleradded t ands for them reset

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,36 @@
 import React, { useRef } from 'react';
+import {useState} from 'react';
 import emailjs from 'emailjs-com';
 
 export const Contact = () => {
   const form = useRef();
+  const [nameValue, setNameValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+  const [messageValue, setMessageValue] = useState("");
+
+  const handleName = (e) => {
+    setNameValue(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmailValue(e.target.value);
+  };
+  const handleMessage = (e) => {
+    setMessageValue(e.target.value);
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
     
-    emailjs.sendForm('healthyLifestyle', 'contact_healthyLifestyle',form.current, 'user_iHEKSYbKPnoYyob6LLTdv')
+    emailjs.sendForm('healthyLifestyle', 'contact_healthyLifestyle',form.current, `${process.env.REACT_APP_UserID}`)
       .then((result) => {
           console.log(result.text);
-         
+          alert("Email Sent!");
+          setNameValue('')
+          setEmailValue('')
+          setMessageValue('')
+
       }, (error) => {
+          alert("Error, Try Again.")
           console.log(error.text);
       });
   };
@@ -20,13 +39,13 @@ export const Contact = () => {
       <h1>Contact Us</h1>
       <form onSubmit={sendEmail} ref={form}>
       <label>Name:</label>
-      <input type="text" name="form_name"/>
+      <input type="text" name="form_name" value={nameValue}  onChange={handleName}/>
       <br/><br/>
       <label>Email:</label>
-      <input type="email" name="user_email"/>
+      <input type="email" name="user_email" value={emailValue} onChange={handleEmail}/>
       <br/><br/>
       <label>Message:</label>
-      <textarea name="message" />
+      <textarea name="message" value={messageValue} onChange={handleMessage}/>
       <input type="submit" name="Send" value="Send"/>
       </form>
     </div>
@@ -34,4 +53,3 @@ export const Contact = () => {
 };
 
 export default Contact;
-
