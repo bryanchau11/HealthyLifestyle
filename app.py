bcc282bb0a5606aa36dff7cf87a342c338cc7ff6
Christopher Lavey <clavey1@student.gsu.edu>
2021-11-07 17:04:47 -0500

    setup google login callback

--- app.py
+++ app.py
@@ -196,13 +196,13 @@ def login():
 def callback():
     # Get authorization code Google sent back to you
     code = flask.request.get("code")
-    
+
     # Find out what URL to hit to get tokens that allow you to ask for
     # things on behalf of a user
     google_provider_cfg = get_google_provider_cfg()
     token_endpoint = google_provider_cfg["token_endpoint"]
 
-    # Prepare and send a request to get tokens! Yay tokens!
+    # Prepare and send a request to get tokens
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
@@ -216,8 +216,36 @@ def callback():
         auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
     )
 
-    # Parse the tokens!
+    # Parse the tokens
     client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Find/hit URL for Google user's profile image and email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_name = userinfo_response.json()["given_name"]
+        picture = userinfo_response.json()["picture"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    user = User.query.filter_by(id=unique_id, username=users_name).first()
+    if user:
+        login_user(user)
+        return flask.redirect(flask.url_for("bp.index"))
+    
+    new_user = User(
+        id = unique_id,
+        username = users_name,
+        password = generate_password_hash("This is a google account use google login instead", sha256)
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    login_user(new_user)
+    return flask.redirect(flask.url_for("bp.index"))
 
 @app.route("/user", methods=["PUT"])
 @login_required
