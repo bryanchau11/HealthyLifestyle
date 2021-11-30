c5ba49cc3e2b23034a58028653dd24a8de2e11af
SidneyWilliams <77303687+SidneyWilliams@users.noreply.github.com>
2021-11-29 20:25:04 -0500

    corrected landing page

--- src/App.js
+++ src/App.js
@@ -33,95 +33,96 @@ quote-props,
 // import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import './Landing.css';
 import Landing1 from './img/landing1.png';
 import Landing2 from './img/landing2.png';
 import Landing3 from './img/landing3.png';
-// import Header from './img/header.png';
+import Header from './img/header.png';
 
 function App() {
   return (
-    <>
-      <Container className="head1">
-        <Row>
-          <Col>
-            <Row>
-              <h1 className="text-center mb-5 mt-5 prime-color">Welcome to Healthy LifeStyle!</h1>
-            </Row>
-            <Row>
-              <h1 className="text-center">
-                Discover the <span className="prime-color">flavors</span>
-                <br />
-                <span className="style-change">
-                  of <span className="prime-color">food</span>fun
-                </span>
-              </h1>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-      <Container className="header" style={{ height: '80vh' }}>
-        <Row style={{ backgroundColor: 'black', color: ' wheat' }}>
-          <Col>
-            <section class="showcase">
-              <div class="container-fluid p-0">
-                <div class="row no-gutters">
-                  <div class="col-lg-6 order-lg-2 text-white showcase-img">
-                    <img src={Landing1} style={{ maxHeight: '100%', maxWidth: '100%' }} alt="header" />
-                  </div>
-                  <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-                    <h2>What functionality is</h2>
-                    <p class="lead mb-0">Content To be added</p>
-                  </div>
+    <div className="centered">
+      <Row>
+        <Col>
+          <Row>
+            <h1 className="text-center mb-5 mt-5" style={{ color: 'wheat' }}>Welcome to Healthy LifeStyle!</h1>
+          </Row>
+          <Row>
+            <h1 className="text-center" style={{ color: 'orange' }}>
+              Discover the <span style={{ color: 'wheat' }}>flavors</span>
+              <br />
+              <span className="style-change" style={{ color: 'orange' }}>
+                of <span style={{ color: 'wheat' }}>food</span>fun
+              </span>
+            </h1>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <div class="col" style={{ backgroundColor: 'black' }}>
+          <img src={Header} style={{ maxHeight: '100%', maxWidth: '100%' }} alt="Header" />
+        </div>
+      </Row>
+      <Row style={{ backgroundColor: 'black', color: ' wheat' }}>
+        <Col>
+          <section class="showcase">
+            <div class="container-fluid p-0">
+              <div class="row no-gutters">
+                <div class="col-lg-6 order-lg-2 text-white showcase-img">
+                  <img src={Landing1} style={{ maxHeight: '100%', maxWidth: '100%' }} alt="Header" />
                 </div>
-                <div class="row no-gutters">
-                  <div class="col-lg-6 text-white showcase-img">
-                    <img src={Landing2} style={{ maxHeight: '100%', maxWidth: '100%' }} alt="header" />
-                  </div>
-                  <div class="col-lg-6 my-auto showcase-text">
-                    <h2>Why it matter</h2>
-                    <p class="lead mb-0">Content To be added</p>
-                  </div>
-                </div>
-                <div class="row no-gutters">
-                  <div class="col-lg-6 order-lg-2 text-white showcase-img">
-                    <img src={Landing3} style={{ maxHeight: '100%', maxWidth: '100%' }} alt="header" />
-                  </div>
-                  <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-                    <h2>Our Meal Plans</h2>
-                    <p class="lead mb-0">Content To be added</p>
-                  </div>
+                <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                  <h2>What functionality is</h2>
+                  <p class="lead mb-0">Content To be added</p>
                 </div>
               </div>
-            </section>
-          </Col>
-        </Row>
-        <Row style={{ backgroundColor: 'black', color: ' wheat' }}>
-          <h2>Who It Is Made By</h2>
-          <Col>
-            <Row>
-              <p className="text-center mb-5 mt-5 prime-color">Name 1</p>
-            </Row>
-          </Col>
-          <Col>
-            <Row>
-              <p className="text-center mb-5 mt-5 prime-color">Name 2</p>
-            </Row>
-          </Col>
-          <Col>
-            <Row>
-              <p className="text-center mb-5 mt-5 prime-color">Name 3</p>
-            </Row>
-          </Col>
-          <Col>
-            <Row>
-              <p className="text-center mb-5 mt-5 prime-color">Name 4</p>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    </>
+              <div class="row no-gutters">
+                <div class="col-lg-6 text-white showcase-img">
+                  <img src={Landing2} style={{ maxHeight: '100%', maxWidth: '100%' }} alt="Header" />
+                </div>
+                <div class="col-lg-6 my-auto showcase-text">
+                  <h2>Why it matter</h2>
+                  <p class="lead mb-0">Content To be added</p>
+                </div>
+              </div>
+              <div class="row no-gutters">
+                <div class="col-lg-6 order-lg-2 text-white showcase-img">
+                  <img src={Landing3} style={{ maxHeight: '100%', maxWidth: '100%' }} alt="Header" />
+                </div>
+                <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                  <h2>Our Meal Plans</h2>
+                  <p class="lead mb-0">Content To be added</p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </Col>
+      </Row>
+      <Row style={{ backgroundColor: 'black', color: ' wheat' }}>
+        <h2>Who It Is Made By</h2>
+        <Col>
+          <Row>
+            <p className="text-center mb-5 mt-5 prime-color">Bryan Chau</p>
+          </Row>
+        </Col>
+        <Col>
+          <Row>
+            <p className="text-center mb-5 mt-5 prime-color">Mahdsiah Khaalik</p>
+          </Row>
+        </Col>
+        <Col>
+          <Row>
+            <p className="text-center mb-5 mt-5 prime-color">Christopher Lavey</p>
+          </Row>
+        </Col>
+        <Col>
+          <Row>
+            <p className="text-center mb-5 mt-5 prime-color">Sidney Williams</p>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   );
 }
 export default App;
